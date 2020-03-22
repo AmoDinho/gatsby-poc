4f7e6a030f356cddfdd5418f43802871757e06f6
AmoDinho <u14284783@tuks.co.za>
2020-03-22 18:23:45 +0200

    added pageQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -17,5 +17,22 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id {
+            frontmatter {
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
