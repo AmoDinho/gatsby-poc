7c743fc27245c05b37afda65625e387dd9df4579
AmoDinho <u14284783@tuks.co.za>
2020-03-22 18:29:13 +0200

    Created a list of posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,19 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {edges.map(edge => (
+      <div key={edge.node.id}>
+        <Link to={edge.node.frontmatter.path}>
+          {edge.node.frontmatter.title}
+        </Link>
+      </div>
+    ))}
   </Layout>
 )
 
@@ -23,11 +26,9 @@ export const pageQuery = graphql`
     allMarkdownRemark {
       edges {
         node {
-          id {
-            frontmatter {
-              path
-              title
-            }
+          frontmatter {
+            path
+            title
           }
         }
       }
